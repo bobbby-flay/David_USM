bda6d34703db8540123b79a3f02d7fe418f5ad6e
Zach <49506494+bobbby-flay@users.noreply.github.com>
2024-03-27 17:15:40 -0500

    Add files via upload
    updated

--- StationReport.m
+++ StationReport.m
@@ -31,31 +31,12 @@ if exist(radiometry_path, 'file') == 2
     trios_ramses = radiometry.trios_ramses;
 end
 %%
+stationN = lisstvsfStationInfo_1.filename;
 figure('Position', [0, 0, 1920, 1080])
 nRows = 2;
 nCols = 3;
 t = tiledlayout(nRows, nCols);
-title(t, 'Sample Report');
-
-nexttile;
-plot(ctdSampleRunJD, ctdSample(:,2))
-title('CTD Data Over Time')
-xlabel('Time (Julian Date)')
-ylabel('CTD Data') 
-ax = gca;
-ax.XAxis.Exponent = 0;
-
-nexttile;
-plot(acsDeltaTimeSeconds, ct)
-title('CT Over Time')
-xlabel('Delta time')
-ylabel('CT')
-
-nexttile;
-plot(acsDeltaTimeSeconds,at)
-title('AT Over Time')
-xlabel('Delta Time')
-ylabel('AT')
+title(t, ['Station: ' stationN]);
 
 nexttile;
 plot(acsDeltaTimeSeconds, ExternalTemperature) %NaN in the sample file
@@ -95,21 +76,13 @@ end
 title('Edp/Eop')
 hold off;
 
-toc
-%%
-plot(ctdSample(:,1), ctdSample(:,2))
-title('CTD Data Over Time')
-xlabel('Salinity')
-ylabel('Temperature (C)') 
-ax = gca;
-ax.XAxis.Exponent = 0;
-%%
+nexttile;
+hold on;
 num_wL = size(at, 2); 
 start_wL = 400; 
 end_wL = 700; 
 wavelengths = linspace(start_wL, end_wL, num_wL);
 
-figure;
 hold on;
 for i = 1:size(at, 1) 
     plot(wavelengths, at(i, :), 'LineWidth', 2);
@@ -119,3 +92,42 @@ hold off;
 xlabel('Wavelength (nm)');
 ylabel('a(m^-^1)');
 title('AC-s');
+hold off;
+
+nexttile;
+hold on;
+yyaxis left
+plot(ctdSampleRunJD, ctdSample(:,2))
+xlabel('Time (JD)')
+ylabel('Temperature (C)')
+title('Temperature and Salinity over Time')
+ax = gca;
+ax.XAxis.Exponent = 0;
+
+yyaxis right
+plot(ctdSampleRunJD, ctdSample(:,1))
+ylabel('Salinity')
+
+hold off;
+
+nexttile;
+hold on;
+T = ctdSample(:,2);
+S = ctdSample(:,1);
+p0 = 999.842594+6.793952e-2.*T-9.095290e-3.*T.^2+1.001685e-4.*T.^3-1.120083e-6.*T.^4+6.536336e-9.*T.^5;
+A = 8.24493e-1-4.0899e-3.*T+7.6438e-5.*T.^2-8.2467e-7.*T.^3+5.3875e-9.*T.^4;
+B = 5.72466e-3+1.0227e-4.*T-1.6546e-6.*T.^2;
+C = 4.8314e-4;
+
+density = p0 + (A.*S) + (B.*S).^32 + (C.*S);
+
+
+
+
+scatter(T, S, 50, density, 'filled');
+colorbar;
+ylabel('Salinity');
+xlabel('Temperature (°C)');
+title('T-S Diagram');
+hold off;
+
